5fb963b5d73c6952cf8f7e5d0a2c1b2452d5a459
Daxar <53128344+Daxar@users.noreply.github.com>
2019-09-23 00:12:11 +0300

    Bugfix

--- Controller/Adminhtml/Pmeds/Edit.php
+++ Controller/Adminhtml/Pmeds/Edit.php
@@ -46,11 +46,7 @@ class Edit extends AbstractAction
         if ($id = $this->getRequest()->getParam('id')) {
             $model = $this->repo->getById($id);
             if (is_string($model->getOptions())) {
-<<<<<<< Updated upstream
-                $model->setOptions(unserialize($model->getOptions()));
-=======
                 $model->setOptions(json_decode($model->getOptions(), true));
->>>>>>> Stashed changes
             }
             $this->dataPersistor->set(Config::DATA_PERSISTOR_OPTIONS_KEY, $model->getOptions());
             $resultPage->getConfig()->getTitle()->prepend(__('Edit question'));
