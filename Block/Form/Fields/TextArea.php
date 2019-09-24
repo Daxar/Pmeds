b2711464b28b0a0ce019fe488252688c1c3f4855
Daxar <53128344+Daxar@users.noreply.github.com>
2019-09-25 00:39:21 +0300

    Delete TextArea.php

--- Block/Form/Fields/TextArea.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-namespace Tingle\Pmeds\Block\Form\Fields;
-
-class TextArea extends AbstractField
-{
-    const TYPE = 'textarea';
-
-    protected $_template = 'Tingle_Pmeds::questions/form/textarea.phtml';
-}
